309cf1447777159e7f88ad58041de65d5223edae
pelirodri <pelirodri@icloud.com>
2023-01-25 02:22:19 -0300

    Add the option to play again

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 import { playForNumberOfRounds } from "./rock-paper-scissors/game.js";
 
-window.onload = () => {
+function play() {
 	// Pass an argument with the number of rounds; defaults to 5
 	playForNumberOfRounds();
+
+	if (confirm("Wanna go again?")) {
+		play();
+	}
+}
+
+window.onload = () => {
+	play();
 };
